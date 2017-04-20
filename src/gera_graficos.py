84320bae0bd306b07cb14f6743858b0d1f5d6e51
hassakura <helio.assakura@gmail.com>
2017-04-19 23:24:23 -0300

    montando graficos

--- src/gera_graficos.py
+++ src/gera_graficos.py
@@ -6,23 +6,54 @@ import matplotlib.pyplot as plt
 import sys
 import math
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+input_size = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+
+#Grafico mandelbrot_seq
+
+fig = plt.figure(1)
 
 fig.suptitle('1 Thread')
 plt.xlabel('Tamanho da entrada')
 plt.ylabel('Tempo gasto')
 
-X = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
-Y = [0.001013716, 0.000875231, 0.001174499, 0.003097618, 0.010853464, 0.041283986, 0.160114177, 0.637433311, 2.524890527, 9.625289915]
-lbl = ['16', '32', '64', '128', '256', '512', '1024', '2048', '4096', '8192']
-for xy in zip(X, Y):                                       # <--
-    ax.annotate('%s' % xy[1], xy = xy, textcoords='data') # <--
-plt.scatter(X, Y, marker='o', s=20)
+
+seq_elephant = [0.000911988, 0.001947579, 0.005810300, 0.021446828, 0.084371574, 0.335577493, 1.335835770, 5.505320427, 21.618584923, 88.499974297]
+seq_full = [0.004342094, 0.001383223, 0.003042832, 0.004556242, 0.016193319, 0.066050534, 0.262612604, 1.022417810, 4.340282105, 17.951102282]
+seq_seahorse = [0.000922286, 0.001980260, 0.006083679, 0.022501505, 0.089204788, 0.353246800, 1.414779058, 5.853969199, 23.400544848, 93.346398082]
+seq_triple_spiral = [0.001118846, 0.002167216, 0.006845384, 0.025581389, 0.100892618, 0.400364173, 1.598103541, 6.405259670, 26.707453872, 108.143847179]
+
+plt.scatter(input_size, seq_elephant, marker='o', s=20)
+plt.scatter(input_size, seq_full, marker='p', s=20)
+plt.scatter(input_size, seq_seahorse, marker='o', s=20)
+plt.scatter(input_size, seq_triple_spiral, marker='o', s=20)
+
+plt.xlim( (2**3, 2**14) )
 plt.xscale('symlog', basex = 2)
-plt.yticks(np.arange(min(Y), max(Y), 0.5))
+#plt.yticks(np.arange(min(seq_elephant), max(seq_elephant), 5))
 plt.grid()
-plt.plot(X,Y)
+fig.add_subplot(111)
+plt.plot(input_size,seq_elephant)
+fig.add_subplot(111)
+plt.plot(input_size,seq_full)
+fig.add_subplot(111)
+plt.plot(input_size,seq_seahorse)
+fig.add_subplot(111)
+plt.plot(input_size,seq_triple_spiral)
 plt.show()
 
+#Grafico teste2
+
+fig_teste2 = plt.figure()
+axteste = fig_teste2.add_subplot(111)
+
+fig_teste2.suptitle("teste2")
+seq_values_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for xy in zip(input_size, seq_values_2):                                       # <--
+    ax.annotate('%s' % xy[1], xy = xy, textcoords='data') # <--
+plt.scatter(input_size, seq_values_2, marker='o', s=20)
+plt.xscale('symlog', basex = 2)
+plt.yticks(np.arange(min(seq_values_2), max(seq_values_2), 0.5))
+plt.grid()
+plt.plot(input_size,seq_values_2)
+plt.show()
 
