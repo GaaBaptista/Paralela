883f9250f8d494344f68f8d3accc48701a4f991a
hassakura <helio.assakura@gmail.com>
2017-04-21 22:52:06 -0300

    relatorio

--- src/Plotador/g_full_pthread.py
+++ src/Plotador/g_full_pthread.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 fig= plt.subplots()
 plt.title('Full Picture pthread')
 plt.xlabel('Tamanho da entrada')
-plt.ylabel('Tempo gasto')
+plt.ylabel('Tempo gasto (s)')
 
 input_size = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
 
@@ -15,15 +15,29 @@ pth_full_8 = [0.001160374, 0.001070056, 0.001120028, 0.002236987, 0.005752060, 0
 pth_full_16 = [0.001057673, 0.001065751, 0.001199627, 0.001691212, 0.003972538, 0.012906732, 0.053638852, 0.206835360, 0.814769476, 3.275115899]
 pth_full_32 = [0.001423940, 0.001537306, 0.001673173, 0.002031913, 0.003663288, 0.009821873, 0.037002377, 0.146378578, 0.609411606, 2.404019988]
 
+stdd_pth_full_1_pct = [90.03, 0.71, 0.93, 0.33, 0.12, 0.11, 0.04, 0.03, 0.01, 0.02]
+stdd_pth_full_2_pct = [2.95, 1.65, 2.12, 1.05, 0.78, 0.45, 0.89, 0.80, 0.18, 0.81]
+stdd_pth_full_4_pct = [1.62, 1.66, 1.03, 0.69, 0.28, 0.05, 0.75, 0.31, 0.80, 1.12]
+stdd_pth_full_8_pct = [4.09, 2.98, 2.05, 2.36, 0.53, 0.29, 0.61, 0.21, 0.18, 0.16]
+stdd_pth_full_16_pct = [2.32, 2.00, 2.12, 1.75, 1.54, 0.63, 2.15, 0.55, 0.54, 0.34]
+stdd_pth_full_32_pct = [2.08, 3.17, 2.40, 1.81, 1.09, 2.84, 2.73, 0.68, 0.91, 0.50]
+
+stdd_pth_full_1 = [(pth_full_1[x] * stdd_pth_full_1_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_full_2 = [(pth_full_2[x] * stdd_pth_full_2_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_full_4 = [(pth_full_4[x] * stdd_pth_full_4_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_full_8 = [(pth_full_8[x] * stdd_pth_full_8_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_full_16 = [(pth_full_16[x] * stdd_pth_full_16_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_full_32 = [(pth_full_32[x] * stdd_pth_full_32_pct[x] / 100) for x in range (0, 10)]
+
 bar_full_width = 1 / 7
 index = np.arange(10)
 
-bar_full_1 = plt.bar(index, pth_full_1, bar_full_width, label = '1 thread')
-bar_full_2 = plt.bar(index + bar_full_width, pth_full_2, bar_full_width, label = '2 threads')
-bar_full_4 = plt.bar(index + 2 * bar_full_width, pth_full_4, bar_full_width, label = '4 threads')
-bar_full_8 = plt.bar(index + 3 * bar_full_width, pth_full_8, bar_full_width, label = '8 threads')
-bar_full_16 = plt.bar(index + 4 * bar_full_width, pth_full_16, bar_full_width, label = '16 threads')
-bar_full_32 = plt.bar(index + 5 * bar_full_width, pth_full_32, bar_full_width, label = '32 threads')
+bar_full_1 = plt.bar(index, pth_full_1, bar_full_width, label = '1 thread', yerr = stdd_pth_full_1)
+bar_full_2 = plt.bar(index + bar_full_width, pth_full_2, bar_full_width, label = '2 threads', yerr = stdd_pth_full_2)
+bar_full_4 = plt.bar(index + 2 * bar_full_width, pth_full_4, bar_full_width, label = '4 threads', yerr = stdd_pth_full_4)
+bar_full_8 = plt.bar(index + 3 * bar_full_width, pth_full_8, bar_full_width, label = '8 threads', yerr = stdd_pth_full_8)
+bar_full_16 = plt.bar(index + 4 * bar_full_width, pth_full_16, bar_full_width, label = '16 threads', yerr = stdd_pth_full_16)
+bar_full_32 = plt.bar(index + 5 * bar_full_width, pth_full_32, bar_full_width, label = '32 threads', yerr = stdd_pth_full_32)
 
 plt.xticks(index + 2.5 * bar_full_width, input_size)
 plt.legend(fontsize = 'x-large')
--- src/Plotador/g_triple_spiral_pthread.py
+++ src/Plotador/g_triple_spiral_pthread.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 fig= plt.subplots()
 plt.title('Triple Spiral Valley pthread')
 plt.xlabel('Tamanho da entrada')
-plt.ylabel('Tempo gasto')
+plt.ylabel('Tempo gasto (s)')
 
 input_size = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
 
@@ -15,15 +15,29 @@ pth_triple_spiral_8 = [0.001207627, 0.001188089, 0.001827614, 0.004895684, 0.016
 pth_triple_spiral_16 = [0.001060821, 0.001286565, 0.001743480, 0.004487179, 0.015828581, 0.066943383, 0.273606375, 1.090377972, 4.365142488, 17.255059378]
 pth_triple_spiral_32 = [0.001478282, 0.001595763, 0.002094780, 0.004584579, 0.014506852, 0.069449128, 0.270402826, 1.072163211, 4.274765938, 17.110603862]
 
+stdd_pth_triple_spiral_1_pct = [0.78, 0.56, 0.33, 0.13, 0.08, 0.02, 0.01, 0.02, 0.01, 0.39]
+stdd_pth_triple_spiral_2_pct = [3.42, 2.18, 0.82, 0.40, 0.56, 0.69, 1.36, 0.09, 0.17, 0.95]
+stdd_pth_triple_spiral_4_pct = [1.67, 1.60, 0.79, 0.28, 0.08, 0.41, 1.98, 1.86, 2.07, 1.44]
+stdd_pth_triple_spiral_8_pct = [8.92, 1.93, 2.52, 2.86, 2.48, 0.67, 0.09, 0.09, 0.45, 0.15]
+stdd_pth_triple_spiral_16_pct = [1.03, 1.83, 2.63, 2.65, 2.02, 2.93, 0.26, 0.14, 0.43, 0.11]
+stdd_pth_triple_spiral_32_pct = [2.13, 2.11, 1.31, 0.45, 0.26, 0.55, 0.11, 0.10, 0.04, 0.06]
+
+stdd_pth_triple_spiral_1 = [(pth_triple_spiral_1[x] * stdd_pth_triple_spiral_1_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_triple_spiral_2 = [(pth_triple_spiral_2[x] * stdd_pth_triple_spiral_2_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_triple_spiral_4 = [(pth_triple_spiral_4[x] * stdd_pth_triple_spiral_4_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_triple_spiral_8 = [(pth_triple_spiral_8[x] * stdd_pth_triple_spiral_8_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_triple_spiral_16 = [(pth_triple_spiral_16[x] * stdd_pth_triple_spiral_16_pct[x] / 100) for x in range (0, 10)]
+stdd_pth_triple_spiral_32 = [(pth_triple_spiral_32[x] * stdd_pth_triple_spiral_32_pct[x] / 100) for x in range (0, 10)]
+
 bar_triple_spiral_width = 1 / 7
 index = np.arange(10)
 
-bar_triple_spiral_1 = plt.bar(index, pth_triple_spiral_1, bar_triple_spiral_width, label = '1 thread')
-bar_triple_spiral_2 = plt.bar(index + bar_triple_spiral_width, pth_triple_spiral_2, bar_triple_spiral_width, label = '2 threads')
-bar_triple_spiral_4 = plt.bar(index + 2 * bar_triple_spiral_width, pth_triple_spiral_4, bar_triple_spiral_width, label = '4 threads')
-bar_triple_spiral_8 = plt.bar(index + 3 * bar_triple_spiral_width, pth_triple_spiral_8, bar_triple_spiral_width, label = '8 threads')
-bar_triple_spiral_16 = plt.bar(index + 4 * bar_triple_spiral_width, pth_triple_spiral_16, bar_triple_spiral_width, label = '16 threads')
-bar_triple_spiral_32 = plt.bar(index + 5 * bar_triple_spiral_width, pth_triple_spiral_32, bar_triple_spiral_width, label = '32 threads')
+bar_triple_spiral_1 = plt.bar(index, pth_triple_spiral_1, bar_triple_spiral_width, label = '1 thread', yerr = stdd_pth_triple_spiral_1)
+bar_triple_spiral_2 = plt.bar(index + bar_triple_spiral_width, pth_triple_spiral_2, bar_triple_spiral_width, label = '2 threads', yerr = stdd_pth_triple_spiral_2)
+bar_triple_spiral_4 = plt.bar(index + 2 * bar_triple_spiral_width, pth_triple_spiral_4, bar_triple_spiral_width, label = '4 threads', yerr = stdd_pth_triple_spiral_4)
+bar_triple_spiral_8 = plt.bar(index + 3 * bar_triple_spiral_width, pth_triple_spiral_8, bar_triple_spiral_width, label = '8 threads', yerr = stdd_pth_triple_spiral_8)
+bar_triple_spiral_16 = plt.bar(index + 4 * bar_triple_spiral_width, pth_triple_spiral_16, bar_triple_spiral_width, label = '16 threads', yerr = stdd_pth_triple_spiral_16)
+bar_triple_spiral_32 = plt.bar(index + 5 * bar_triple_spiral_width, pth_triple_spiral_32, bar_triple_spiral_width, label = '32 threads', yerr = stdd_pth_triple_spiral_32)
 
 plt.xticks(index + 2.5 * bar_triple_spiral_width, input_size)
 plt.legend(fontsize = 'x-large')
